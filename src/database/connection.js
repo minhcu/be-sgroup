6e3d1c75573e45e73de8560851afe769aa71d716
minhcu <minhdong1831999@gmail.com>
2023-04-15 01:54:12 +0700

    env

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mysql = require('mysql')
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config()
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '12345678',
-    database: 'be',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 })
 
 connection.connect((err) => {
