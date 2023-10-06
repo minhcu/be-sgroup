b86899e3a1cde2666661bc50ef47fcde442ad88a
minhcu <minhdong1831999@gmail.com>
2023-10-06 21:07:13 +0700

    fix: change to mysql2

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,14 +1,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-// const mysql = require('mysql')
-// eslint-disable-next-line import/no-extraneous-dependencies
 require('dotenv').config()
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 const connection = require('knex')({
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
         host: process.env.DB_HOST,
-        port: 3306,
+        port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
