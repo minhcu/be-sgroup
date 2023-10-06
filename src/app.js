41520a3f3a9d12b6c72cf0c3f7ef082b7b2843fe
minhcu <minhdong1831999@gmail.com>
2023-10-06 21:05:59 +0700

    fix add cors and validator

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 // const crypto = require('crypto')
 
 // const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
 //     modulusLength: 2048,
 // })
 // const morgan = require('morgan')
+
+const { validateToken } = require('./middlewares/tokenValidate')
 
 const app = express()
 const port = 3000
@@ -15,6 +18,24 @@ const pollRoute = require('./poll')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use((req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true)
+
+    // Pass to next layer of middleware
+    next()
+})
 
 function key(req, res, next) {
     req.key = 'secret'
@@ -22,8 +43,8 @@ function key(req, res, next) {
 }
 
 app.use('/auth', key, authRoute)
-app.use('/users', key, userRoute)
-app.use('/polls', key, pollRoute)
+app.use('/users', key, validateToken, userRoute)
+app.use('/polls', key, validateToken, pollRoute)
 
 app.use('/', (req, res) => res.send('Hello World!'))
 
